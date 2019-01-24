56fe036236e4e0ffc395cacf4d6313f9102b8ee4
Daanish Nasir <daanishnasir@gmail.com>
2019-01-24 16:48:56 -0600

    Saving work before label component

--- src/lib/input/input.component.ts
+++ src/lib/input/input.component.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SpinnerComponent } from '../spinner';
 
 @Component({
   selector: 'cui-input',
-  template: `
-    <p>
-      input works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './input.component.html',
+  styles: [],
 })
 export class InputComponent implements OnInit {
+
+  @Input() public className: string;
+  @Input() public inputClassName: string;
+  @Input() public inputSize: string;
+  @Input() public readOnly: boolean = false;
+  @Input() public disabled: boolean = false;
+  @Input() public theme: string;
+  @Input() public value: string | number;  //Initial text value
+  @Input() public placeholder: string;
+
+  @Input() public errorType;
+  @Input() public nestedLevel;
+
+
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  get wrapperClasses() {
+    return {
+      [this.inputSize + ' columns']: this.inputSize,
+      ['read-only']: this.readOnly,
+      ['disabled']: this.disabled,
+      ['cui-input-group--' + this.theme]: this.theme,
+      [this.errorType]: this.errorType,
+      ['cui-input--nested-' + this.nestedLevel]: this.nestedLevel,
+      [this.className]: this.className,
+    };
+  }
+
+  get inputClasses() {
+    return {
+      [this.inputSize + ' columns']: this.inputSize,
+      ['cui-input-group--' + this.theme]: this.theme,
+      [this.errorType]: this.errorType,
+      ['cui-input--nested-' + this.nestedLevel]: this.nestedLevel,
+
+      [this.inputClassName]: this.inputClassName,
+      ['read-only']: this.readOnly,
+      ['disabled']: this.disabled,
+      ['dirty']: this.value
+    };
+  }
+
+
+
+
 
 }
--- src/lib/input/input.module.ts
+++ src/lib/input/input.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input.component';
+import { SpinnerModule } from '../spinner';
+
+
 
 @NgModule({
   declarations: [InputComponent],
   imports: [
-    CommonModule
+    CommonModule, SpinnerModule
   ],
   exports: [InputComponent]
 })
